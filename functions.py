ecfd7d8eca24e50add30564137fbf0c23cdf9a34
pawanpreet <pawan.baweja@gmail.com>
2022-12-29 12:41:06 +0530

    Create basic GUI without interactions Day 16

--- functions.py
+++ functions.py
@@ -1,8 +1,12 @@
-def get_todos():
-    with open('todos.txt', 'r') as file:
+FILEPATH = 'todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file:
         todos_local = file.readlines()
     return todos_local
 
-def write_todos(todos_arg):
-    with open('todos.txt', 'w') as file:
+
+def write_todos(todos_arg,filepath=FILEPATH):
+    with open(filepath, 'w') as file:
         file.writelines(todos_arg)
