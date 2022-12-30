f54ab21d42cea31f8fd6dccb17ffbf664d6abc47
pawanpreet <pawan.baweja@gmail.com>
2022-12-30 13:43:27 +0530

    implemented exit and complete

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[[label],[input_box,add_button],[list_box,edit_button]]],
+                   layout=[[[label],
+                            [input_box,add_button],
+                            [list_box,edit_button, complete_button],
+                            [exit_button]]],
                    font = ('Helvetica' , 16))
 
 
@@ -25,6 +30,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
 
         case "Edit":
             todo_to_edit = value['todos'][0]
@@ -36,12 +42,23 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
 
+        case "Complete":
+            todo_to_complete = value['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case 'Exit':
+            break
+
         case 'todos':
             window['todo'].update(value=value['todos'][0])
         case sg.WIN_CLOSED:
-            break
+            exit()
 
 
 
-
+print("Bye!")
 window.close()
